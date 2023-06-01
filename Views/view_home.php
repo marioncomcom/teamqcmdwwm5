925053aa83cd1a40090dc704d7c0ff1e94b4416c
jordan13100 <jordan.babdor@gmail.com>
2023-06-01 14:49:22 +0200

    Page home ok

--- Views/view_home.php
+++ Views/view_home.php
@@ -1,5 +1,38 @@
 <!-- <a href="?controller=home&action=home">home</a> -->
 
 
-<a href="?controller=home&action=connexion">connexion</a>
-<a href="?controller=home&action=inscription">inscription</a>
+
+<main class="fond_rose_home">
+
+<nav class="navbartitre" style="background-color: #FF3859;">
+
+    
+  
+
+<img src="Content/images/logo.png" alt=""> 
+</nav>
+
+<div class="color_p">
+<p>Bienvenue sur notre page d'accueil, l'endroit idéal pour les amateurs de cinéma,
+    de quiz cérébraux et de sport ! Préparez-vous à tester vos connaissances et à mettre à l'épreuve votre esprit vif.
+     </p>
+        </div>
+
+
+
+
+<div class="connexion_connected">
+
+<a href="?controller=home&action=connexion" class="btn btn-primary btn-block" style="background-color: #FFF; border: black">Connexion</a>
+
+</div>
+
+
+
+<div class="inscription_connected">
+
+
+<a href="?controller=home&action=inscription" class="btn btn-primary btn-block" style="background-color: #FFF; border: black">Inscription</a>
+</div>
+</main>    
+
